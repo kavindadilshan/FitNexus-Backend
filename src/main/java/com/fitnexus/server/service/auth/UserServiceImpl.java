ed00e680f83fde0b115cc010d4b62a05e626186a
kavindadilshan <kavindadilshan97@gmail.com>
2024-02-02 19:05:47 +0530

    added condition for check user

--- src/main/java/com/fitnexus/server/service/auth/UserServiceImpl.java
+++ src/main/java/com/fitnexus/server/service/auth/UserServiceImpl.java
@@ -1,52 +1,94 @@
-package com.fitnexus.server.service;
+package com.fitnexus.server.service.auth;
 
 import com.fitnexus.server.config.security.custom.CustomOauthException;
-import com.fitnexus.server.dto.AuthUserDetailsDTO;
-import com.fitnexus.server.dto.UserAuthDTO;
-import com.fitnexus.server.entity.AuthUser;
+import com.fitnexus.server.constant.FitNexusConstants;
+import com.fitnexus.server.dto.auth.AuthUserDetailsDTO;
+import com.fitnexus.server.dto.auth.PublicAuthUserDetailsDTO;
+import com.fitnexus.server.dto.auth.UserAuthDTO;
+import com.fitnexus.server.dto.exception.CustomServiceException;
+import com.fitnexus.server.entity.auth.AuthUser;
+import com.fitnexus.server.entity.auth.EmailVerificationToken;
+import com.fitnexus.server.entity.auth.ForgotPasswordVerificationToken;
+import com.fitnexus.server.entity.auth.UserRoleDetail;
+import com.fitnexus.server.entity.businessprofile.BusinessAgreement;
+import com.fitnexus.server.entity.businessprofile.BusinessProfile;
+import com.fitnexus.server.entity.businessprofile.BusinessProfileManager;
+import com.fitnexus.server.entity.instructor.Instructor;
+import com.fitnexus.server.entity.publicuser.PublicUser;
+import com.fitnexus.server.entity.trainer.Trainer;
+import com.fitnexus.server.enums.BusinessAgreementStatus;
+import com.fitnexus.server.enums.ManagerStatus;
 import com.fitnexus.server.enums.UserRole;
-import com.fitnexus.server.repository.AuthUserRepository;
+import com.fitnexus.server.enums.UserStatus;
+import com.fitnexus.server.repository.auth.AuthUserRepository;
+import com.fitnexus.server.repository.auth.EmailVerificationTokenRepository;
+import com.fitnexus.server.repository.auth.ForgotPasswordVerificationTokenRepository;
+import com.fitnexus.server.repository.businessprofile.BusinessAgreementRepository;
+import com.fitnexus.server.repository.businessprofile.BusinessProfileRepository;
+import com.fitnexus.server.repository.publicuser.PublicUserRepository;
+import com.fitnexus.server.util.CustomGenerator;
+import com.fitnexus.server.util.EmailSender;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collections;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
-/**
- * Created by :- Intellij Idea
- * Author :- Tharindu
- * Date :- 2020-04-17
- */
+import static com.fitnexus.server.config.security.SecurityConstants.*;
+import static com.fitnexus.server.constant.FitNexusConstants.DetailConstants.MAX_USER_LOGIN_ATTEMPTS;
+import static com.fitnexus.server.constant.FitNexusConstants.DetailConstants.USER_LOCK_PERIOD;
+import static com.fitnexus.server.constant.FitNexusConstants.NotFoundConstants.NO_BUSINESS_PROFILE_FOUND;
+
+
 @Service
 @RequiredArgsConstructor
-@Log4j2
-public class UserServiceImpl implements UserService{
+@Slf4j
+public class UserServiceImpl implements UserService {
 
     private final AuthUserRepository authUserRepository;
+    private final PublicUserRepository publicUserRepository;
 
-    private final PasswordEncoder passwordEncoder;
-    private final ModelMapper modelMapper;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+    private AuthUser getAuthUser(String username) {
         AuthUser authUser = authUserRepository.findByUsername(username)
-                .orElseThrow(() -> new CustomOauthException("Invalid username"));
-        return new UserAuthDTO(authUser.getId(), username, authUser.getPassword(), getRole(authUser.getUserRole()),
-                authUser.getStatus(), modelMapper.map(authUser, AuthUserDetailsDTO.class));
+                .orElseThrow(() -> new CustomOauthException("You have entered an invalid username or password"));
+        if (authUser.getStatus() == UserStatus.INACTIVE || authUser.getStatus() == UserStatus.INACTIVE_PENDING) {
+            throw new CustomOauthException("This user account is inactive. Please contract FitNexus support.");
+        }
+        checkLoginAttempts(authUser, null);
+        return authUser;
     }
 
-    /**
-     * @param userRole the user role of a searched user
-     * @return the user role as authority
-     */
-    private List<SimpleGrantedAuthority> getRole(UserRole userRole) {
-        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + userRole));
+    private PublicUser getPublicUser(String mobile) {
+        PublicUser publicUser = publicUserRepository.findByMobile(mobile);
+        if (publicUser == null) {
+            log.error("Invalid mobile");
+            throw new CustomOauthException("You have entered an invalid username or password");
+        }
+        if (publicUser.getStatus() == UserStatus.INACTIVE) {
+            throw new CustomOauthException("This user account is inactive. Please contract FitNexus support.");
+        }
+
+        return publicUser;
     }
+
+
 
 }
+
