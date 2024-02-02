a815e968821568ab44258e522df91e401f51fa12
kavindadilshan <kavindadilshan97@gmail.com>
2024-02-02 11:25:47 +0530

    create constance files

---
+++ src/main/java/com/fitnexus/server/constant/FitNexusConstants.java
@@ -0,0 +1,19 @@
+package com.fitnexus.server.constant;
+
+import java.math.BigDecimal;
+
+
+public class FitNexusConstants {
+
+    public static final class ErrorConstants {
+        public static final String SERVICE_ERROR = "Service Error, please contact support!";
+        public static final String INVALID_OLD_PASS = "Invalid old password";
+        public static final String SMS_PASSCODE = "Q2V5ZW50cmFUZWNobm9sb2dpZXNDb2xsYWJvcmF0ZVdpdGhMaW9uVG91cnNAMTIzNDU=";
+        public static final String SMS_CODE_NOT_ALLOWED = "Invalid SMS passcode!";
+        public static final String USER_NOT_ACTIVATED = "User not activated!";
+        public static final String FORBIDDEN_RESOURCE = "You are not authorized to access this resource!";
+        public static final String BUSINESS_PROFILE_EXPIRED = "Business profile expired!";
+        public static final int TOO_MANY_REQUESTS = 429;
+    }
+
+}
